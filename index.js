47c26dd8ccbc180093b4b4a9c2347b87597fa249
zxcdet <84541557+zxcdet@users.noreply.github.com>
2024-05-24 11:29:03 +0300

    feature: Add style new js file new img

--- index.js
+++ index.js
@@ -6,28 +6,25 @@ const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
  const server = createServer((req, res) => {
-     if (req.url === '/first-click') {
-         readFile(path.join(__dirname, 'first.html'), (err, data) => {
-             if (err) throw err;
-             res.end(data);
-         });
-         return
-     }
-     if (req.url === '/second-click') {
-         readFile(path.join(__dirname, 'second.html'), (err, data) => {
-             if (err) throw err;
-             res.end(data);
-         });
-         return;
-     }
-     if (req.url.match(/.jpg$/)) {
+     if (req.url.match(/.css$/)) {
+         res.setHeader('Content-type', 'text/css');
          createReadStream(path.join(__dirname, req.url)).pipe(res)
+     }
+     if (req.url.match(/.(pg|jpeg|png|gif|webp)$/)) {
          res.setHeader('Content-Type', 'image/jpg');
+         createReadStream(path.join(__dirname, req.url)).pipe(res)
+     }
+     if (req.url.match(/.js$/)) {
+         res.setHeader('Content-type', 'text/javascript');
+         createReadStream(path.join(__dirname, req.url)).pipe(res)
      }
      readFile(path.join(__dirname, 'index.html'), (err, data) => {
         if (err) throw err;
          res.end(data);
     });
+     if (req.url === '/data') {
+         res.end(JSON.stringify({name: 'data'}));
+     }
  });
 
 server.listen(3000, '127.0.0.1', () => {
